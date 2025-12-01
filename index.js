813062d6479ca2f5ff1a3b17914ffb7dc832c06b
dwainXDL <personal.dwain@gmail.com>
2025-12-01 16:41:19 +0530

    Add CI/CD workflow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
